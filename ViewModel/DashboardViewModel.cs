84b9dfc8195f7db84d369fde41400ff66af52bfc
TomTomXD <tomasvlcek.pce@gmail.com>
2024-10-30 17:58:56 +0100

    KlientDashboard Logout button

--- ViewModel/DashboardViewModel.cs
+++ ViewModel/DashboardViewModel.cs
@@ -20,38 +20,65 @@ namespace InformacniSystemBanky.ViewModel
         }
 
         public ICommand ShowHomeCommand { get; }
-        public ICommand ShowSettingsCommand { get; }
+        //public ICommand ShowSettingsCommand { get; }
         public ICommand ShowProfileCommand { get; }
         public ICommand LogoClickedCommand { get; }
+        public ICommand ShowAccountsCommand { get; }
+        public ICommand LogOutCommand { get; }
 
         public DashboardViewModel()
         {
             ShowHomeCommand = new RelayCommand(ShowHome);
-            ShowSettingsCommand = new RelayCommand(ShowSettings);
+            //ShowSettingsCommand = new RelayCommand(ShowSettings);
             ShowProfileCommand = new RelayCommand(ShowProfile);
             LogoClickedCommand = new RelayCommand(ShowHome);
+            ShowAccountsCommand = new RelayCommand(ShowAccounts);
+            LogOutCommand = new RelayCommand(LogOut);
 
             // Výchozí pohled
             //CurrentViewModel = new HomeViewModel(); // Změněno na HomeViewModel
+        }
+
+        private void LogOut()
+        {
+            // Otevření nového přihlašovacího okna (LoginView)
+            var loginView = new LoginView();
+            loginView.Show();
+
+            // Zavření aktuálního okna (DashboardView)
+            var currentWindow = Application.Current.Windows
+                .OfType<DashboardKlientView>()
+                .FirstOrDefault();
+
+            if (currentWindow != null)
+            {
+                currentWindow.Close();
+            }
         }
 
         private void ShowHome()
         {
             //CurrentViewModel = new HomeViewModel();
             //CurrentViewModel = new DashboardKlientView();
-            MessageBox.Show("Logo clicked");
+            //MessageBox.Show("Logo clicked");
         }
 
-        private void ShowSettings()
-        {
-            CurrentViewModel = new HomeView();
-            MessageBox.Show("Settings");
-        }
+        //private void ShowSettings()
+        //{
+        //    CurrentViewModel = new HomeView();
+        //    MessageBox.Show("Settings");
+        //}
 
         private void ShowProfile()
         {
             //CurrentViewModel = new ProfileViewModel();
             MessageBox.Show("Profile");
+        }
+
+        private void ShowAccounts()
+        {
+            CurrentViewModel = new AccountsView();
+            MessageBox.Show("Accounts clicked");
         }
 
         // INotifyPropertyChanged implementace
